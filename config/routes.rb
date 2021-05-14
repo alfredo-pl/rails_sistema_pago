5d71fbd5f92902e263a7dd83af7fb726960769ce
Alfredo Peña <aalfredoplz@gmail.com>
2021-05-14 10:18:11 -0400

    views payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :images
   resources :products
+  resources :images
+  resources :physicals
+  resources :digitals
   resources :profiles
+  resources :orders
+  resources :payments
+  resources :payment_methods
+
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
